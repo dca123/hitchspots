70dab9ee297778270050d93698a8723edb686e00
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-05-13 10:50:20 -0500

    feat: :sparkles: Added Location Card model to update state

---
+++ lib/models/location_card.dart
@@ -0,0 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+
+class LocationCardModel extends ChangeNotifier {
+  String _locationName = "I-20 Exit";
+  double _locationRating = 0.0;
+  late String _locationID = "testLocationID";
+  String get locationName => _locationName;
+  double get locationRating => _locationRating;
+  Map<String, dynamic> _reviews = {};
+  List get reviews => _reviews.values.toList();
+
+  void updateLocation(dynamic locationData, String locationID) {
+    if (_locationID != locationID) {
+      _reviews.clear();
+      _locationName = locationData['name'];
+      _locationRating = locationData['rating'].toDouble();
+      _locationID = locationID;
+    }
+    print(_locationID);
+    notifyListeners();
+  }
+
+  void getReviews() async {
+    var reviewQuery = await FirebaseFirestore.instance
+        .collection("reviews")
+        .limit(2)
+        .where("locationID", isEqualTo: _locationID)
+        .get();
+    reviewQuery.docs.forEach((document) {
+      _reviews[document.id] = document.data();
+    });
+    print(reviews);
+    notifyListeners();
+  }
+
+  void clearReviews() {
+    _reviews.clear();
+  }
+}
