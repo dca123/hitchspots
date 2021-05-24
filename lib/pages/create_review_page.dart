7ef62e1b6953f7a28bf37c74080eb17134f7dcca
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-05-24 02:58:00 -0500

    feat(pages/create_review_page): Uses new RatingsBar Form widget

--- lib/pages/create_review_page.dart
+++ lib/pages/create_review_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import '../widgets/form_fields/rating_bar.dart';
+import 'package:hitchspots/widgets/form_fields/rating_bar.dart';
 import '../models/location_card.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -67,49 +68,15 @@ class CreateReviewPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(height: 24),
-                FormField<double>(
-                  builder: (ratingFormContext) {
-                    return Column(
-                      children: [
-                        RatingBar(
-                          initialRating: 0,
-                          glow: false,
-                          itemPadding: EdgeInsets.symmetric(horizontal: 8.0),
-                          allowHalfRating: true,
-                          ratingWidget: RatingWidget(
-                            full: Icon(Icons.star, color: Colors.yellow[700]),
-                            half: Icon(Icons.star_half,
-                                color: Colors.yellow[700]),
-                            empty: Icon(Icons.star_outline,
-                                color: Colors.yellow[700]),
-                          ),
-                          onRatingUpdate: (value) =>
-                              ratingFormContext.setValue(value),
-                        ),
-                        if (ratingFormContext.hasError)
-                          Column(
-                            children: [
-                              SizedBox(height: 16),
-                              Text(
-                                ratingFormContext.errorText!,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .caption!
-                                    .apply(color: Theme.of(context).errorColor),
-                              ),
-                            ],
-                          ),
-                      ],
-                    );
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return 'Please select a rating';
-                    }
-                    ratingController = value;
-                    return null;
-                  },
-                ),
+                RatingBarFormField(
+                    buildContext: context,
+                    validator: (value) {
+                      if (value == null) {
+                        return 'Please select a rating';
+                      }
+                      ratingController = value;
+                      return null;
+                    }),
                 SizedBox(height: 24),
                 TextFormField(
                   controller: descriptionTextController,
