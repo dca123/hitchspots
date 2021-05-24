dccf50387820b5bbf204a5b2be5999f9433652f2
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-05-24 02:58:47 -0500

    feat(lib/pages/create_location_page.dart): :sparkles: Locations can be added to app

--- lib/pages/create_location_page.dart
+++ lib/pages/create_location_page.dart
@@ -1,17 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:geoflutterfire2/geoflutterfire2.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../widgets/form_fields/rating_bar.dart';
+import '../widgets/form_fields/location_picker.dart';
 // import 'package:cloud_firestore/cloud_firestore.dart';
 
-class CreateLocationPage extends StatelessWidget {
-  const CreateLocationPage({
-    Key? key,
-  }) : super(key: key);
-
+class CreateLocationPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    void addLocation() {
-      // FirebaseFirestore.instance
-      //     .collection('locations')
-      //     .add({'text': 'testMessage'})
+  _CreateLocationPageState createState() => _CreateLocationPageState();
+}
+
+class _CreateLocationPageState extends State<CreateLocationPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController locationName = TextEditingController();
+  final TextEditingController locationExperience = TextEditingController();
+  double ratingController = 0;
+  late LatLng position;
+  final geo = GeoFlutterFire();
+
+  void addLocation() async {
+    if (_formKey.currentState!.validate()) {
+      GeoFirePoint newSpot =
+          geo.point(latitude: position.latitude, longitude: position.longitude);
+      print(locationName.text);
+      print(locationExperience.text);
+      print(ratingController);
+      print(position);
+      final locationID =
+          await FirebaseFirestore.instance.collection('locations').add({
+        'name': locationName.text,
+        'position': newSpot.data,
+        'rating': ratingController,
+      });
+
+      FirebaseFirestore.instance.collection('reviews').add({
+        'description': locationExperience.text,
+        'locationID': locationID.id,
+        'rating': ratingController,
+      });
+
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: const Text('Thank you for contributing!'),
@@ -21,7 +49,16 @@ class CreateLocationPage extends StatelessWidget {
         Navigator.pop(context);
       });
     }
+  }
 
+  @override
+  void dispose() {
+    locationName.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).canvasColor,
@@ -49,67 +86,59 @@ class CreateLocationPage extends StatelessWidget {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(children: [
+            MapLocationFormField(
+              buildContext: context,
+              onSaved: (value) => position = value,
+            ),
+            SizedBox(height: 24),
+            RatingBarFormField(
+                buildContext: context,
+                validator: (value) {
+                  if (value == null) {
+                    return 'Please select a rating';
+                  }
+                  ratingController = value;
+                  return null;
+                }),
+            SizedBox(height: 24),
+            TextFormField(
+              controller: locationName,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: "Location",
+                labelText: "Add name",
+                hintText: "Simple Location Description",
               ),
             ),
-            Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(
-                  Icons.star_border_outlined,
-                  size: 36,
-                ),
-                Icon(
-                  Icons.star_border_outlined,
-                  size: 36,
-                ),
-                Icon(
-                  Icons.star_border_outlined,
-                  size: 36,
-                ),
-                Icon(
-                  Icons.star_border_outlined,
-                  size: 36,
-                ),
-                Icon(
-                  Icons.star_border_outlined,
-                  size: 36,
-                ),
-              ],
-            ),
-            TextField(
+            SizedBox(height: 24),
+            TextFormField(
+              controller: locationExperience,
               decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Add name",
-                  hintText: "Simple Location Description"),
-            ),
-            TextField(
-              decoration: InputDecoration(
+                  alignLabelWithHint: true,
                   border: OutlineInputBorder(),
                   labelText: "Experience",
-                  hintText: "Your Experience"),
+                  helperText: "How long did you wait ? Many vehicles go by ?",
+                  hintText: "Describe your experience briefly."),
+              maxLines: 3,
               keyboardType: TextInputType.multiline,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
             ),
-            OutlinedButton(
-                onPressed: () => {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.camera_enhance),
-                    Text("Add Photos"),
-                  ],
-                ))
-          ],
+          ]),
         ),
       ),
     );
