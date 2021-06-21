342ccc19cd00ba5a6cfad907bd920956075680cf
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-06-21 22:32:26 +0530

    feat: Checks if image exists

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -3,7 +3,16 @@ import * as functions from "firebase-functions";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const doesImageExist = async (latitude: number, longitude: number): Promise<boolean> => {
+    const mapDataResponse = await axios.get("https://maps.googleapis.com/maps/api/streetview/metadata", {
+        params: {
+            location: `${latitude},${longitude}`,
+            key: functions.config().map_api_key,
+        },
+    });
+    if (mapDataResponse.data["status"] === "OK") {
+        return true;
+    }
+    return false;
+};
 });
