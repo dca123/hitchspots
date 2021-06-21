81e750fd99e23aa11a4944d3c5df343d14d9fbde
Devinda Senanayaka <dca123@users.noreply.github.com>
2021-06-21 22:33:14 +0530

    feat(firebase/functions/src/index.ts): :sparkles: Downloads image

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -15,4 +15,30 @@ const doesImageExist = async (latitude: number, longitude: number): Promise<bool
     }
     return false;
 };
+const downloadImage = async (locationID: number, latitude: number, longitude: number, heading: number) => {
+    if (!existsSync(resolve(tmpdir(), "gmaps_static_images", `${locationID}`))) {
+        mkdirSync(resolve(tmpdir(), "gmaps_static_images", `${locationID}`));
+    }
+    const path = resolve(tmpdir(), "gmaps_static_images", `${locationID}`, `${heading}.jpeg`);
+    const writer = createWriteStream(path);
+    const response = await axios({
+        url: "https://maps.googleapis.com/maps/api/streetview",
+        params: {
+            location: `${latitude},${longitude}`,
+            size: "411x411",
+            heading: heading,
+            fov: 120,
+            key: functions.config().map_api_key,
+        },
+        responseType: "stream",
+        method: "GET",
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+        writer.on("finish", resolve);
+        writer.on("error", reject);
+    });
+};
 });
