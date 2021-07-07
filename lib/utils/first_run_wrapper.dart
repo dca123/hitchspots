b73d2430eb4fd3e6119ab0b2e167e0a428b9dbcd
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2021-07-07 16:54:35 +0530

    feat(utils/first_run_wrapper): First run wrapper now uses a future builder to check first run

--- lib/utils/first_run_wrapper.dart
+++ lib/utils/first_run_wrapper.dart
@@ -2,41 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:hitchspots/pages/onboard_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class FirstRunWrapper extends StatefulWidget {
+class FirstRunWrapper extends StatelessWidget {
   FirstRunWrapper({Key? key, required this.homePage}) : super(key: key);
+  Future<bool> _checkIfFirstRun() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstRun = (prefs.getBool('isFirstRun')) ?? true;
+    return isFirstRun;
+  }
 
   final Widget homePage;
   @override
-  _FirstRunWrapperState createState() => _FirstRunWrapperState();
-}
-
-class _FirstRunWrapperState extends State<FirstRunWrapper> {
-  bool _hasLoadedRunState = false;
-  late bool _isFirstRun;
-
-  Future<void> _checkIfFirstRun() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool isFirstRun = (prefs.getBool('isFirstRun')) ?? true;
-    if (isFirstRun) {
-      await prefs.setBool('isFirstRun', false);
-    }
-    setState(() {
-      _hasLoadedRunState = true;
-      _isFirstRun = isFirstRun;
-    });
-  }
-
-  _FirstRunWrapperState() {
-    _checkIfFirstRun();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Container(
-      child: _hasLoadedRunState
-          ? (_isFirstRun ? OnboardingPage() : widget.homePage)
-          // TODO: SplashScreen here
-          : Container(),
-    );
+        child: FutureBuilder<bool>(
+      future: _checkIfFirstRun(),
+      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            if (snapshot.data == true) {
+              return OnboardingPage();
+            } else {
+              return homePage;
+            }
+          default:
+            return Container(child: Text("Loading Cache Data"));
+        }
+      },
+    ));
   }
 }
