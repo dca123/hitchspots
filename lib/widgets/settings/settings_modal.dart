e6e178903daec67737bfd883337af8e9ea6ecf65
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2021-07-08 13:51:47 +0530

    feat(widgets/settings/settings_modal): :lipstick: Create responsive settings modal

---
+++ lib/widgets/settings/settings_modal.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class SettingsCard extends StatelessWidget {
+  const SettingsCard({
+    Key? key,
+  }) : super(key: key);
+
+  Widget titleWidget(context) => Flexible(
+        flex: 1,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Flexible(
+              flex: 2,
+              child: Text(
+                "Settings",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Text(
+                "Devinda Senanayake",
+                style: Theme.of(context).textTheme.caption,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.50,
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  titleWidget(context),
+                  // SizedBox(height: 16),
+                  Expanded(
+                    flex: 4,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("Edit Profile"),
+                          leading: Icon(Icons.manage_accounts),
+                          onTap: () => {},
+                        ),
+                        ListTile(
+                          title: Text("Offline Mode"),
+                          leading: Icon(Icons.download_for_offline),
+                          onTap: () => {},
+                        ),
+                        ListTile(
+                          title: Text("Logout"),
+                          leading: Icon(Icons.logout),
+                          onTap: () => {},
+                        ),
+                        ListTile(
+                          title: Text("About"),
+                          leading: Icon(Icons.info),
+                          onTap: () => {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
