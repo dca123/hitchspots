6f33c594d97fb8b4e2e902b6a3c13738d9106454
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2021-07-16 23:00:36 +0530

    feat(pages/spash_screen_dart): :sparkles: Added splash screen

---
+++ lib/pages/splash_screen_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width * 0.785,
+          color: Colors.white,
+          child: Flexible(
+              child: RiveAnimation.asset(
+            "assets/splash/globe.riv",
+          ))),
+    );
+  }
+}
